f42a789ae5ebcbb6bc03853db118411aa3ec50fa
Antoine <ahoarau@polytech.upmc.fr>
2011-04-07 20:52:50 +0200

    Debut des fonctions InterfaceX, ajout du sprite

--- include/InterfaceX.h
+++ include/InterfaceX.h
@@ -1,6 +1,9 @@
 #ifndef INTERFACEX_H_INCLUDED
 #define INTERFACEX_H_INCLUDED
-
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <SDL_ttf.h>
 
 class InterfaceX
 {
@@ -14,9 +17,10 @@ protected:
     TTF_Font *_font;
     SDL_Color _textColor;
 public:
-   // InterfaceX(int nbJoueurs):_SREEN_WIDTH
-
-
+    // InterfaceX(int nbJoueurs):_SREEN_WIDTH
+    SDL_Surface* load_image( std::string filename );
+    void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip);
+    bool init();
 
 };
 #endif // INTERFACEX_H_INCLUDED
