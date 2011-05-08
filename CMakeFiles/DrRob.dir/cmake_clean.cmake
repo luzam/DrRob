47d2b8dbe86aa63583a40fa23bdf2d39fbbfeb2b
Antoine <ahoarau@polytech.upmc.fr>
2011-05-08 15:57:08 +0200

    commit pour linux

---
+++ CMakeFiles/DrRob.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DrRob.dir/source/DashBoard.o"
+  "CMakeFiles/DrRob.dir/source/Game.o"
+  "CMakeFiles/DrRob.dir/source/Grille.o"
+  "CMakeFiles/DrRob.dir/source/InterfaceX.o"
+  "CMakeFiles/DrRob.dir/source/MoteurPhy.o"
+  "CMakeFiles/DrRob.dir/source/SDL_rotozoom.o"
+  "CMakeFiles/DrRob.dir/main.o"
+  "DrRob.app/Contents/MacOS/DrRob.pdb"
+  "DrRob.app/Contents/MacOS/DrRob"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/DrRob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
